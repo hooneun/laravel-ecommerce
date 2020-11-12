47287d313313310c2e39aaebb20580f2eb7e9905
Hoon <kimhoon0316@gmail.com>
2020-11-12 14:36:26 +0900

    inertia, vue3 install

---
+++ resources/js/Pages/Users/Index.vue
@@ -0,0 +1,24 @@
+<template>
+  <div v-for="(user, index) in state.users" :key="index">
+    {{ index }} {{ user.name }}
+  </div>
+</template>
+
+<script>
+import { reactive } from "vue";
+export default {
+  props: ["users", "test"],
+  setup(props) {
+    console.log(props);
+    const state = reactive({
+      users: [],
+    });
+
+    state.users = props.users;
+
+    return {
+      state,
+    };
+  },
+};
+</script>
